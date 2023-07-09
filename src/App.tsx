c307b24c14026214a5bd82c8a6e44c947ffb689a
sirwhod <rodrigo.brandao98@gmail.com>
2023-07-09 12:47:26 -0300

    Correção no deploy

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import { ThemeProvider } from 'styled-components'
 import { defaultTheme } from './styles/themes/default'
-import { darkTheme } from './styles/themes/dark'
 import { BrowserRouter } from 'react-router-dom'
 import { GlobalStyle } from './global'
 import { Router } from './Router'
